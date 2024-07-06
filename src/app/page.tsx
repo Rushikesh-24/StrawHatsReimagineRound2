0dd1390c37c184cd6f191fcf95e0799b92e0c2bf
Rushikesh-24 <rushikeshgaonkar24@gmail.com>
2024-07-06 21:39:07 +0530

    Updated Animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="w-screen overflow-x-hidden bg-gray-700 h-screen">
+    <div className="w-screen overflow-x-hidden bg-white h-screen">
     </div>
   );
 }
--- src/components/CameraAnimation.tsx
+++ src/components/CameraAnimation.tsx
@@ -25,11 +25,11 @@ const CameraAnimation: React.FC<CameraAnimationProps> = ({ children }) => {
     const sequence = async () => {
       await controls.start({ opacity: 1, scale: 0.4, transition: { duration: 2,delay:1.2 } });
       setShowFlash(true);
-      await controls.start({ opacity: 1, transition: { duration: 0.5 } });
-      await controls.start({ opacity: 0, transition: { duration: 0.22 } });
+      await controls.start({ opacity: 1, transition: { duration: 0.7 } });
+      await controls.start({ opacity: 0, transition: { duration: 0.7  } });
       setShowFlash(false);
       setZoomInLens(true);
-      await new Promise(resolve => setTimeout(resolve, 3000)); // Delay to show zoom effect
+      await new Promise(resolve => setTimeout(resolve, 1000)); // Delay to show zoom effect
       setShowContent(true);
     };
     sequence();
@@ -37,9 +37,13 @@ const CameraAnimation: React.FC<CameraAnimationProps> = ({ children }) => {
 
   if (showContent) {
     return (
-      <div className="relative flex justify-center items-center h-screen bg-black">
+      <motion.div className="relative flex justify-center items-center h-screen bg-black"
+      initial={{opacity: 0}}
+      animate={{opacity: [0,0.1,0.2,0.3,0.4,0.6,0.7,0.8,0.9,1]}}
+      transition={{duration:2,ease:'easeInOut'}}
+      >
         {children}
-      </div>
+      </motion.div>
     );
   }
 
@@ -59,17 +63,18 @@ const CameraAnimation: React.FC<CameraAnimationProps> = ({ children }) => {
         <motion.div
           className="absolute inset-0 bg-white z-10 blur-sm"
           initial={{ opacity:0 , scale:1 }}
-          animate={{ opacity: 1,scale:1 }}
-          transition={{ duration: 0.1, repeat:1, repeatType:"reverse" }}
+          animate={{ opacity: [1, 0.8, 0.6, 0.4, 0],scale:1 }}
+          transition={{ duration: 0.1, repeat:1, repeatType:"reverse",opacity:[0,0.4,0.6,0.8,1] }}
         />
       )}
       <motion.div
         className={`${zoomInLens ? 'absolute -inset-36 z-20 transform' : 'relative'} flex justify-start`}
         initial={{ opacity: 0, scale: 0.2 }}
-        animate={zoomInLens ? { opacity: 0.85, scale: 3.9,x:-1000,y:330 } : controls}
-        transition={{ duration: 1 }}
+        animate={zoomInLens ? { opacity: 0.85, scale: 4,x:-1000,y:330 } : controls}
+        transition={{ duration: 1 , opacity: 0.3}}
       >
-        <Image src={camera} alt="Camera" layout={zoomInLens ? "fill" : "intrinsic"} objectFit={zoomInLens ? "cover" : "contain"} className={`${zoomInLens ? '-inset-44':''}`}/>
+        <Image src={camera} alt="Camera" layout={zoomInLens ? "fill" : "intrinsic"} objectFit={zoomInLens ? "cover" : "contain"} className={`${zoomInLens ? '-inset-44':''}`} loading='eager'/>
+        <div className={`absolute inset-0 bg-black ${zoomInLens ? 'bg-opacity-65':'bg-opacity-80 '}`}></div>
       </motion.div>
     </div>
   );
