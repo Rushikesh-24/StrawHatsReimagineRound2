7a0cf4a57083168769390baec62367d6ea9b4c72
Rushikesh-24 <rushikeshgaonkar24@gmail.com>
2024-07-17 22:54:11 +0530

    updated the code

--- src/components/CanonModel.tsx
+++ src/components/CanonModel.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Canvas, useLoader, useFrame } from "@react-three/fiber";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { Suspense } from "react";
 import * as THREE from "three";
-import { Plane, useTexture } from "@react-three/drei";
-import image from '../../public/canonBg.png'
 import { useSpring, a } from "@react-spring/three";
+import { easeInOut, motion } from "framer-motion";
 
 interface MeshComponentProps {
   rotation: number;
@@ -20,13 +19,12 @@ interface ModelViewerProps {
 export const MeshComponent: React.FC<MeshComponentProps> = ({ rotation })=> {
   const fileUrl = "canon1.glb";
   const gltf = useLoader(GLTFLoader, fileUrl);
-  const meshRef = useRef<React.MutableRefObject<THREE.Object3D>>(null);
+  const meshRef = useRef<THREE.Object3D>(null);
   const mouse = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
-  const texture = useTexture(image.src);
 
   const { rotationSpring } = useSpring({
     rotationSpring: rotation,
-    config: { mass: 1, tension: 170, friction: 26 },
+    config: { mass: 1, tension: 170, friction: 26 }
   });
 
   useEffect(() => {
@@ -69,26 +67,47 @@ export const MeshComponent: React.FC<MeshComponentProps> = ({ rotation })=> {
       rotation-x={Math.PI / 8.1}
       rotation-y={rotationSpring as unknown as number}
     />
-    <Plane args={[10, 10]} position={[3, 2, 1]} rotation={[Math.PI / 2, 0, 0]}>
-        <meshBasicMaterial attach="material" map={texture} />
-      </Plane>
     </>
   ) : null;
 }
 // Math.PI/1.4*2.5
 export const ModelViewer: React.FC<ModelViewerProps> = ({rotation}) => {
+  const [animationComplete, setAnimationComplete] = useState(false);
+  const { rotationSpring } = useSpring({
+    rotationSpring: rotation,
+    config: { mass: 1, tension: 170, friction: 26 },
+    onRest: () => {
+      if (rotation === Math.PI / 1.4 * 2.5) {
+        setAnimationComplete(true);
+      }
+    },
+  });
+
   return (
-    <div className="md:flex justify-center items-center lg:h-screen lg:w-screen md:w-[80vw] md:h-[80vh] hidden z-20 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      <Canvas
-        camera={{ position: [60, 70, -50], fov: 3}}
-      >
-        {/* Change this camera position for different position  */}
-        <Suspense fallback={null}>
-          <ambientLight intensity={1} />
-          <directionalLight position={[0, 1, 0]} intensity={10} />
-          <MeshComponent rotation={rotation}/>
-        </Suspense>
-      </Canvas>
+    <div className="relative z-20 w-screen h-screen">
+      <div className="md:flex justify-center items-center lg:h-screen lg:w-screen md:w-[80vw] md:h-[80vh] hidden absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 overflow-hidden">
+        <Canvas camera={{ position: [60, 70, -50], fov: 3 }}>
+          <Suspense fallback={null}>
+            <ambientLight intensity={1} />
+            <directionalLight position={[0, 1, 0]} intensity={10} />
+            <MeshComponent rotation={rotation} />
+          </Suspense>
+        </Canvas>
+      </div>
+      {animationComplete && (
+        <motion.div
+          className="absolute bg-black lg:w-[12.5rem] lg:h-[10rem] w-44 top-[62%] -rotate-[20deg] lg:top-[65%] left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        >
+          <motion.img
+            src="/canonBg.png"
+            alt="Background"
+            className="w-full h-full object-cover"
+            initial={{ rotate: 0,scale: 1  }}
+            animate={{ rotate: 20,scale:5 }}
+            transition={{ duration: 1 ,delay: 2 }}
+          />
+        </motion.div>
+      )}
     </div>
   );
 };
