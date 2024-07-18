cf92afdd4def65246d7b4b95364f6905dab4afe9
Shridhar <shridharkamat10@gmail.com>
2024-07-19 00:28:47 +0530

    feat: added entrance animation to hanging images

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,10 +9,10 @@ const Hero = () => {
     <div
       className={`h-screen w-screen flex bg-heroimage font-silk relative`}
     >
-      <div className="hover-detect flex justify-start items-center bg-opacity-40 border-white border w-80 h-20 group cursor-pointer absolute bottom-28 right-28 overflow-hidden flex-row hover:bg-white /hover:flex-row-reverse transition-all ease-in-out duration-100 bg-black text-white">
-      <div className="w-0 group-hover:w-1/4 h-full duration-500 group-hover:flex group-hover:translate-y-0 group-hover:translate-x-0 translate-y-full -translate-x-full justify-center items-center stroke-black bg-white  ">
+      <div className="hover-detect flex justify-start items-center bg-opacity-40 border-white border w-80 h-20 group cursor-pointer absolute bottom-10 right-10 overflow-hidden flex-row hover:bg-white /hover:flex-row-reverse transition-all ease-in-out duration-100 bg-black text-white">
+      <div className="w-0 group-hover:w-1/4 h-full duration-500 group-hover:flex group-hover:translate-y-0 group-hover:translate-x-0 translate-y-full -translate-x-full justify-center items-center stroke-black group-hover:bg-black group-hover:stroke-white bg-white  ">
           <svg
-            width="37"
+            width="25"
             height="43"
             viewBox="0 0 37 43"
             fill="none"
@@ -36,12 +36,12 @@ const Hero = () => {
             />
           </svg>
         </div>
-        <div className="text-5xl w-3/4 h-full flex justify-center items-center bg-opacity-40 group-hover:text-black group-hover:bg-white duration-100">
+        <div className="text-[2.5rem] w-3/4 h-full flex justify-center items-center bg-opacity-40 group-hover:text-black group-hover:bg-white duration-100">
           EXPLORE
         </div>
         <div className="w-1/4 h-full group-hover:w-[0%] overflow-hidden flex justify-center items-center stroke-black bg-white  group-hover:stroke-white">
           <svg
-            width="37"
+            width="25"
             height="43"
             viewBox="0 0 37 43"
             fill="none"
