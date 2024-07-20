9e4123a0fe7cd31d3fdb16f97ab731c135b6c97e
Harsh Telang <telangharsh06@gmail.com>
2024-07-20 13:30:42 +0530

    Responsiveness for mobile view added

--- src/components/PrinterCards.tsx
+++ src/components/PrinterCards.tsx
@@ -5,11 +5,10 @@ import {
   useAnimation,
   AnimatePresence,
 } from "framer-motion";
-import { FaCaretLeft } from "react-icons/fa";
-import { FaCaretRight } from "react-icons/fa";
+import { FaCaretLeft, FaCaretRight } from "react-icons/fa";
 import { Silkscreen } from "next/font/google";
 import Link from "next/link";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback, useMemo } from "react";
 import MenuItem from "./MenuItem";
 
 const silkScreen = Silkscreen({
@@ -73,7 +72,7 @@ const PrinterCards = () => {
     const sequence = () => {
       Zoomcontrols.start({
         scale: isOpen ? 50 : 1,
-        transition: { duration: 2 },
+        transition: { duration: 1.5 },
       });
       setPageTransition(true);
     };
@@ -93,13 +92,13 @@ const PrinterCards = () => {
             index < currentIndex ? "PIXMA"[index] : randomLetter()
           )
         );
-      }, 100);
+      }, 75);
 
       initialTimeout = setTimeout(() => {
         clearInterval(randomizeInterval);
         setIsSettling(true);
         setCurrentIndex(0);
-      }, 2000);
+      }, 1500);
     } else {
       // Settling Phase
       settleInterval = setInterval(() => {
@@ -112,7 +111,7 @@ const PrinterCards = () => {
         if (currentIndex === "PIXMA".length) {
           clearInterval(settleInterval);
         }
-      }, 500);
+      }, 400);
 
       // After randomization
       setTimeout(() => {
@@ -132,8 +131,8 @@ const PrinterCards = () => {
           if (currentIndex === "PIXMA".length) {
             clearInterval(finalSettleInterval);
           }
-        }, 500);
-      }, 1000);
+        }, 400);
+      }, 800);
     }
 
     return () => {
@@ -153,19 +152,19 @@ const PrinterCards = () => {
     }
   };
 
-  const handleScroll = () => {
-    setRotationSpeed(1); // Increase rotation speed by 10 times
+  const handleScroll = useCallback(() => {
+    setRotationSpeed(5); // Increase rotation speed
     setTimeout(() => {
       setRotationSpeed(10); // Reset rotation speed after 2 seconds
     }, 2000);
-  };
+  }, []);
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, [handleScroll]);
 
   useEffect(() => {
     controls.start({
@@ -174,35 +173,45 @@ const PrinterCards = () => {
     });
   }, [rotationDirection, controls, rotationSpeed]);
 
+  // const menuItems = useMemo(() => {
+  //   return (
+  //     <>
+  //       <MenuItem href="/menu-item-1">Menu Item 1</MenuItem>
+  //       <MenuItem href="/menu-item-2">Menu Item 2</MenuItem>
+  //       <MenuItem href="/menu-item-3">Menu Item 3</MenuItem>
+  //     </>
+  //   );
+  // }, []);
+
   return (
     <div
-      className={`h-[100vh] w-[95vw] bg-white rounded-3xl flex flex-col items-center relative ${
-        isOpen ? "px-0 pt-10" : "px-5 pt-20"
+      className={`overflow-x-hidden overflow-y-hidden sm:h-[100vh] h-[90vh] w-[95vw] bg-white rounded-3xl flex flex-col items-center relative ${
+        isOpen ? "px-0 sm:pt-0 pt-10" : "px-5 sm:pt-0 pt-5"
       }`}
       onMouseMove={handleMouseMove}
     >
       <div
         className={`${silkScreen.className} font-normal text-left ${
           isOpen ? "sm:text-[10rem]" : "sm:text-[14.5rem]"
-        } text-3xl text-white mix-blend-difference w-full z-10`}
+        } text-5xl text-white mix-blend-difference w-full z-10`}
         style={{ textAlign: "left" }}
       >
         {text.join("")}
       </div>
       <div className="flex justify-center items-center mt-8">
         <motion.div
-          className="relative -top-14 size-[35rem] rounded-full flex items-center justify-center border-b-8 border-black -rotate-[30deg]"
+          className="relative -top-[10%] sm:-top-[22%] size-[35rem] rounded-full items-center justify-center border-b-8 border-black -rotate-[30deg] sm:flex hidden"
           animate={pageTransition ? "" : controls}
         ></motion.div>
         <motion.div
-          className={`size-[30rem] rounded-full bg-black flex flex-col justify-center items-center absolute bottom-[4.8rem]`}
+          className={`sm:size-[30rem] size-96 rounded-full bg-black flex flex-col justify-center items-center absolute bottom-[4.8rem] sm:bottom-10`}
           onClick={handleClick}
           animate={isOpen ? "" : Zoomcontrols}
         >
           <img
             src="/printer1.png"
             alt=""
-            className={`size-96 ${isOpen ? "hidden" : "flex"}`}
+            className={`sm:size-96 size-40 ${isOpen ? "hidden" : "flex"}`}
           />
           <p
             className={`${silkScreen.className} text-white ${
@@ -214,7 +223,7 @@ const PrinterCards = () => {
         </motion.div>
       </div>
       <div
-        className={`${silkScreen.className} font-normal text-right text-black w-full absolute bottom-5 right-5`}
+        className={`${silkScreen.className} font-normal text-right text-black w-full absolute bottom-5 sm:right-5 right-2`}
       >
         SCROLL TO SEE OTHER CATEGORIES
       </div>
@@ -222,13 +231,13 @@ const PrinterCards = () => {
         {isOpen && pageTransition && (
           <>
             <motion.div
-              className="absolute bottom-0 -left-10 transform z-20 bg-white h-[75vh] w-[35vw] flex flex-col gap-14 items-center pt-14"
+              className="absolute bottom-0 -left-10 transform z-20 bg-white h-[75vh] w-[35vw] sm:flex hidden flex-col gap-14 items-center pt-14"
               initial={{ z: -20, opacity: 0 }}
               animate={{ z: 0, opacity: 1 }}
               exit={{ z: -20, opacity: 0 }}
-              transition={{ delay: 0.5, ease: easeInOut }}
+              transition={{ delay: 0.3, ease: easeInOut }}
             >
-              <AnimatePresence wait>
+              <AnimatePresence>
                 <motion.img
                   key={slideIndex}
                   src={slides[slideIndex].image}
@@ -237,7 +246,7 @@ const PrinterCards = () => {
                   initial={{ opacity: 0, x: 50 }}
                   animate={{ opacity: 1, x: 0 }}
                   exit={{ opacity: 0, x: -50 }}
-                  transition={{ duration: 0.5 }}
+                  transition={{ duration: 0.3 }}
                 />
               </AnimatePresence>
               <div
@@ -251,24 +260,24 @@ const PrinterCards = () => {
               </div>
             </motion.div>
             <motion.div
-              className="absolute top-0 -right-10 bg-white h-32 w-[40vw] flex justify-end items-center pr-5"
+              className="absolute sm:top-0 top-[3rem] sm:-right-10 right-2 sm:bg-white sm:h-32 sm:w-[40vw] flex justify-end items-center pr-5 sm:pr-20"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
-              transition={{ delay: 0.5, ease: easeInOut }}
+              transition={{ delay: 0.3, ease: easeInOut }}
             >
               <motion.button
                 onClick={() => {
                   setIsHamOpen(!isHamOpen);
                 }}
                 className={`sm:size-14 size-8 relative flex flex-col sm:gap-1 gap-[2px] justify-center z-50 ${
-                  isHamOpen ? "mt-5" : ""
+                  isHamOpen ? "" : ""
                 }`}
                 animate={isHamOpen ? "open" : "closed"}
               >
                 {/* Hamburger Icon */}
                 <motion.div
-                  className="h-1 sm:w-4 w-2 ml-1 bg-black"
+                  className="h-1 sm:w-4 w-2 ml-1 sm:bg-black bg-white"
                   variants={{
                     open: { scale: 0 },
                     closed: { scale: 1 },
@@ -278,7 +287,7 @@ const PrinterCards = () => {
                   className={`w-full ${
                     isHamOpen
                       ? "rounded-sm sm:h-2 h-1 bg-white"
-                      : "rounded-t-sm h-2 bg-black"
+                      : "rounded-t-sm h-2 bg-white sm:bg-black"
                   }`}
                   variants={{
                     open: { rotate: "45deg" },
@@ -297,21 +306,21 @@ const PrinterCards = () => {
                   }}
                 ></motion.div>
                 <motion.div
-                  className="w-full h-2 bg-black"
+                  className="w-full h-2 sm:bg-black bg-white"
                   variants={{
                     open: { scale: 0 },
                     closed: { scale: 1 },
                   }}
                 ></motion.div>
                 <motion.div
-                  className="w-full h-2 bg-black rounded-b-sm"
+                  className="w-full h-2 sm:bg-black bg-white rounded-b-sm"
                   variants={{
                     open: { scale: 0 },
                     closed: { scale: 1 },
                   }}
                 ></motion.div>
                 <motion.div
-                  className="sm:size-6 size-4 rounded-full bg-black border-2 border-white absolute top-[35%] left-[25%]"
+                  className="sm:size-6 size-4 rounded-full sm:bg-black bg-white border-2 sm:border-white border-black absolute top-[35%] left-[25%]"
                   variants={{
                     open: { scale: 0 },
                     closed: { scale: 1 },
@@ -336,32 +345,33 @@ const PrinterCards = () => {
               </AnimatePresence>
             </motion.div>
             <motion.div
-             className="flex flex-col items-center absolute -right-10 bottom-0 h-[80vh] w-[65vw] pt-14 gap-5"
-             initial={{ z: -20, opacity: 0 }}
-             animate={{ z: 0, opacity: 1 }}
-             exit={{ z: -20, opacity: 0 }}
-             transition={{ delay: 0.5, ease: easeInOut }}>
+              className="flex flex-col items-center absolute sm:-right-10 sm:bottom-0 h-[80vh] w-[65vw] pt-14 gap-5 pl-0 sm:pl-5"
+              initial={{ z: -20, opacity: 0 }}
+              animate={{ z: 0, opacity: 1 }}
+              exit={{ z: -20, opacity: 0 }}
+              transition={{ delay: 0.3, ease: easeInOut }}
+            >
               <div
                 className={`${silkScreen.className} font-normal text-left sm:text-[10rem] text-3xl text-[#434343] mix-blend-difference w-full`}
                 style={{ textAlign: "left" }}
               >
                 G4770
               </div>
-              <AnimatePresence wait>
+              <AnimatePresence>
                 <motion.div
                   key={slideIndex}
-                  className={`flex flex-col gap-5 text-2xl ${silkScreen.className} font-normal text-left h-40 w-full mt-20 pl-4`}
+                  className={`flex flex-col sm:gap-5 gap-2 sm:text-2xl text-xl ${silkScreen.className} font-normal text-left h-40 w-full sm:mt-20 pl-4`}
                   initial={{ opacity: 0, x: 50 }}
                   animate={{ opacity: 1, x: 0 }}
                   exit={{ opacity: 0, x: -50 }}
-                  transition={{ duration: 0.5 }}
+                  transition={{ duration: 0.3 }}
                 >
                   {slides[slideIndex].content.map((text, index) => (
                     <p key={index}>{text}</p>
                   ))}
                 </motion.div>
               </AnimatePresence>
-              <div className="w-full h-28 flex justify-end items-center mt-16 pr-16">
+              <div className="w-full h-28 sm:flex hidden justify-end items-center mt-16 pr-16">
                 <FaCaretLeft className="text-6xl cursor-pointer" onClick={handlePrevious} />
                 <FaCaretRight className="text-6xl cursor-pointer" onClick={handleNext} />
               </div>
