473b85988ecb80ba89886b3ba762cfc2a9611085
Rushikesh-24 <rushikeshgaonkar24@gmail.com>
2024-07-20 13:45:07 +0530

    feat: Animation of About us added

--- src/app/test1/page.tsx
+++ src/app/test1/page.tsx
@@ -1,22 +1,50 @@
 "use client";
 import AboutUs from "@/components/AboutUs";
 import React from "react";
+import { text } from "stream/consumers";
 
 const page = () => {
-    const data = [
-        {title:"ABOUT US",
-            paragraph1:"Canon India Pvt. Ltd. is the sales and marketing subsidiary of Canon Inc., a world leader in imaging technologies",
-            paragraph2:"Canon India markets a comprehensive range of sophisticated contemporary digital imaging products and solutions in India.",
-            paragraph3:"In sync with its corporate tagline- ‘Delighting You Always’, reinforced by World-class technology, Canon offers an extended product portfolio."},{
-                title:"SUSTAINABILITY",
-        paragraph1:"Canon India takes pride to be socially inclined and focused towards its efficient and sustainable CSR projects.",
-        paragraph2:"At Canon, the CSR endeavors are driven by its corporate philosophy of ‘Kyosei’, embodying the spirit of ‘living and working together for common good’. ",
-        paragraph3:"Canon takes pride in not only bringing quality products to the market; but also, in contributing towards minimizing the environmental burden, through the effective application of green technologies."
-            }
-    ]
+  const data = [
+    {
+      title: "ABOUT US",
+      paragraph1:
+        "Canon India Pvt. Ltd. is the sales and marketing subsidiary of Canon Inc., a world leader in imaging technologies",
+      paragraph2:
+        "Canon India markets a comprehensive range of sophisticated contemporary digital imaging products and solutions in India.",
+      paragraph3:
+        "In sync with its corporate tagline- ‘Delighting You Always’, reinforced by World-class technology, Canon offers an extended product portfolio.",
+    },
+    {
+      title: "SUSTAINABILITY",
+      paragraph1:
+        "Canon India takes pride to be socially inclined and focused towards its efficient and sustainable CSR projects.",
+      paragraph2:
+        "At Canon, the CSR endeavors are driven by its corporate philosophy of ‘Kyosei’, embodying the spirit of ‘living and working together for common good’. ",
+      paragraph3:
+        "Canon takes pride in not only bringing quality products to the market; but also, in contributing towards minimizing the environmental burden, through the effective application of green technologies.",
+    },
+    {
+      title: "",
+      paragraph1: "",
+      paragraph2: "",
+      paragraph3: "",
+    },
+    {
+      title: "",
+      paragraph1: "",
+      paragraph2: "",
+      paragraph3: "",
+    },
+    {
+      title: "",
+      paragraph1: "",
+      paragraph2: "",
+      paragraph3: "",
+    },
+  ];
   return (
-    <div className="flex flex-col">
-      <AboutUs data={data}/>
+    <div className="z-10">
+      <AboutUs data={data} />
     </div>
   );
 };
--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -25,28 +25,45 @@ const AboutUs: React.FC<AboutUsProps> = ({ data }) => {
 
   useEffect(() => {
     return scrollYProgress.onChange((progress) => {
-      const newIndex = Math.min(Math.floor(progress * data.length), data.length - 1);
+      const newIndex = Math.min(
+        Math.floor(progress * data.length),
+        data.length - 1
+      );
       setCurrentIndex(newIndex);
     });
   }, [scrollYProgress, data.length]);
 
   useEffect(() => {
-    if (currentIndex === 2) {
-      const timer = setTimeout(() => {
-        setAnimationComplete(true);
-      }, 700); // Adjust delay as necessary
-      return () => clearTimeout(timer);
-    } else {
-      setAnimationComplete(false);
-    }
+    console.log(currentIndex);
   }, [currentIndex]);
 
   return (
-    <div className={`w-screen h-screen overflow-hidden ${silkScreen.className}`}>
-      <div ref={ref} className="w-full h-full overflow-y-scroll">
-        <div className="w-full h-[300vh] flex flex-col justify-center items-center">
-          <div className="fixed top-0 w-full h-screen flex justify-center items-center bg-white">
-            <div className="w-3/4 h-4/6 bg-black rounded-[3rem] relative flex flex-col justify-around p-4">
+    <div
+      className={`w-screen h-screen ${silkScreen.className}`}
+    >
+      <div
+        ref={ref}
+        className="w-full h-screen overflow-y-auto overflow-x-hidden relative"
+      >
+        <div className="w-full h-[400vh] flex flex-col justify-center items-center relative z-20 bg-yellow-300">
+          <motion.div className="fixed pointer-events-none top-0 left-0 w-full h-screen flex justify-center items-center bg-white">
+            <motion.div
+              className={`w-3/4 h-4/6 transition-all duration-700 ease-in-out bg-black rounded-[3rem] relative flex flex-col justify-around p-4 items-center`}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+              style={{
+                width:
+                  currentIndex >= 3 ? "100vw" : "75%",
+                height:
+                  currentIndex >= 3 ? "100vh" : "66.666667%",
+                borderRadius:
+                currentIndex >= 3 ? "0" : "3rem",
+                justifyContent:
+                currentIndex >= 3 ? "center" : "space-around",
+
+              }}
+            >
               <AnimatePresence mode="wait">
                 <motion.h1
                   key={data[currentIndex].title}
@@ -96,19 +113,49 @@ const AboutUs: React.FC<AboutUsProps> = ({ data }) => {
                 </motion.div>
               </AnimatePresence>
               <motion.div
-                className="absolute bg-white mix-blend-difference w-1/3 h-10 -right-20 top-[35%] z-50"
-                initial={{ x: 100 }}
-                animate={{ x: currentIndex === 1 ? 60 : -1000 }}
-                transition={{ duration: 0.5, delay: 0.2, ease: "easeInOut" }}
+                className="absolute bg-white transition-all  ease-in-out mix-blend-difference w-1/3 h-10 -right-20 top-[35%] z-50"
+                initial={{ x: 100, scaleY: 1,opacity: 1,y:0}}
+                animate={{
+                    x:currentIndex === 1 ? 60 : currentIndex == 2 ? -450 : -1000,
+                    scaleY: currentIndex == 2 ? 4.05 :currentIndex>2 ? 0.1: 1,
+                    opacity:currentIndex>2?0:1,
+                }}
+                transition={{ duration: 0.5, delay: 0.1, ease: "easeInOut" }}
+                style={{
+                  mixBlendMode:
+                  currentIndex > 1 ? currentIndex > 2?"color" :"lighten" : "difference",
+                  display:
+                  currentIndex>2?"hidden":"flex"
+                }}
+                
               />
               <motion.div
-                className="absolute bg-white mix-blend-difference w-1/3 h-10 -left-20 bottom-40 z-50"
-                initial={{ x: -100 }}
-                animate={{ x: currentIndex === 1 ? -60 : 1000 }}
-                transition={{ duration: 0.5, delay: 0.2, ease: "easeInOut" }}
+                className="absolute transition-all  ease-in-out bg-white mix-blend-difference w-1/3 h-10 -left-20 bottom-40 z-50"
+                initial={{ x: -100, scaleY: 1 ,y:0,opacity:1}}
+                animate={{
+                  x: currentIndex === 1 ? -60 : currentIndex === 2 ? 445 : 1000,
+                  scaleY: currentIndex == 2 ? 4.05 :currentIndex>2? 0.1: 1,
+                  opacity:currentIndex>2?0:1,
+                }}
+                transition={{ duration: 0.5, delay: 0.1, ease: "easeInOut" }}
+                style={{
+                  mixBlendMode:
+                   currentIndex > 1 ? currentIndex > 2?"color" :"lighten" : "difference",
+                    display:
+                  currentIndex>2?"hidden":"flex"
+                }}
               />
-            </div>
-          </div>
+              {currentIndex>2 &&
+              <motion.div className="bg-white w-full h-1/2 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
+              initial={{opacity: 0.7,width:"20%",height:"50%"}}
+              animate={{opacity: 1,width:"100%",height:currentIndex>3?"100%":"50%"}}
+              transition={{duration:0.5,damping:4,ease:"easeInOut"}}
+              >
+              </motion.div>
+
+              }
+            </motion.div>
+          </motion.div>
         </div>
       </div>
     </div>
