401c67b566606ef5beebf76da1f727b3753db3df
Shridhar <shridharkamat10@gmail.com>
2024-07-25 22:11:13 +0530

    fix: mobile responsiveness issues fixed

--- src/components/CameraCategories.tsx
+++ src/components/CameraCategories.tsx
@@ -5,7 +5,7 @@ import Pic2 from './NewComponents/Pic2'
 function CameraCategories() {
   return (
     <div data-scroll-section className='h-screen w-screen relative grid grid-cols-5 border-t border-b border-white overflow-hidden'>
-        <div className='relative col-span-2'>
+        <div className='relative col-span-5 md:col-span-2'>
             <Image 
                 data-scroll
                 data-scroll-speed={-5}
@@ -16,24 +16,24 @@ function CameraCategories() {
                 className='absolute bottom-[8%]'
             />
         </div>
-        <div className='col-span-3 flex border-l-[20px] border-white h-screen'>
-          <div className='bg-[#8d2a25] h-screen flex-1 duration-500 first-drawer relative flex items-start pt-10 overflow-hidden group'>
-            <p className='absolute right-10 text-[6.5rem]' style={{writingMode:"vertical-rl"}}>
+        <div className='col-span-5 md:col-span-3 flex flex-col md:flex-row md:border-l-[20px] border-white h-screen'>
+          <div className='bg-[#8d2a25] h-screen flex-1 duration-500 md:first-drawer relative flex items-start pt-10 overflow-hidden group'>
+            <p className='md:absolute right-10 text-4xl md:text-[6.5rem] md:[writing-mode:vertical-rl]'>
               Interchangeable <br />
-              <span className='text-4xl absolute bottom-5'>lens camera</span>
+              <span className='text-xl md:text-4xl md:absolute bottom-5'>lens camera</span>
             </p>
             <Pic2 />
           </div>
-          <div className='bg-[#71221e] h-screen flex-1 duration-500 second-drawer relative flex items-start pt-10 overflow-hidden'>
-            <p className='absolute right-10 text-[6.5rem]' style={{writingMode:"vertical-rl"}}>
+          <div className='bg-[#71221e] h-screen flex-1 duration-500 md:second-drawer relative flex items-start pt-10 overflow-hidden'>
+            <p className='md:absolute right-10 text-4xl md:text-[6.5rem] md:[writing-mode:vertical-rl]'>
               Digital <br />
-              <span className='text-4xl absolute bottom-5 uppercase'>lens camera</span>
+              <span className='text-xl md:text-4xl md:absolute bottom-5 uppercase'>lens camera</span>
             </p>
           </div>
-          <div className='bg-[#611a16] h-screen flex-1 duration-500 third-drawer relative flex items-start pt-10 overflow-hidden'>
-            <p className='absolute right-10 text-[6.5rem] uppercase' style={{writingMode:"vertical-rl"}}>
+          <div className='bg-[#611a16] h-screen flex-1 duration-500 md:third-drawer relative flex items-start pt-10 overflow-hidden'>
+            <p className='md:absolute right-10 text-4xl md:text-[6.5rem] md:[writing-mode:vertical-rl] uppercase'>
               LENSES <br />
-              <span className='text-4xl absolute bottom-5'>AND ACCESSORIES</span>
+              <span className='text-xl md:text-4xl md:absolute bottom-5'>AND ACCESSORIES</span>
             </p>
           </div>
         </div>
